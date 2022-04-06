3bc444631969441ef9add72208b2a2a7c27ba301
Jenna <itcodegirl@gmail.com>
2022-04-06 13:28:47 -0500

    Part 1-Databases-Part 2

--- db/index.js
+++ db/index.js
@@ -1,31 +1,29 @@
-// imports the pg module
-const { Client } = require("pg");
-const { rows } = require("pg/lib/defaults");
+const { Client } = require("pg"); // imports the pg module
 
-// supply the db name and location of the database
 const client = new Client("postgres://localhost:5432/juicebox-dev");
 
-// export them
-module.exports = {
-  client,
-  getAllUsers,
-  createUser,
-  updateUser,
-  createPost,
-  updatePost,
-  getAllPosts,
-  getPostsByUser,
-  getPostsById
-};
+/**
+ * USER Methods
+ */
 
-async function getAllUsers() {
-  const { rows } = await client.query(
-    `SELECT id, username, name, location 
-    FROM users;
-  `
-  );
+async function createUser({ username, password, name, location }) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      INSERT INTO users(username, password, name, location) 
+      VALUES($1, $2, $3, $4) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `,
+      [username, password, name, location]
+    );
 
-  return rows;
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function updateUser(id, fields = {}) {
@@ -50,34 +48,20 @@ async function updateUser(id, fields = {}) {
       RETURNING *;
     `,
       Object.values(fields)
-      // this expression returns an array
     );
 
-    return result;
+    return user;
   } catch (error) {
     throw error;
   }
 }
 
-// THE PATTERN - In general, in our db/index.js file we should provide the utility functions that
-// the rest of our application will use.
-// We will call them from the seed file, but also from our main application file.
-// That is where we are going to listen to the front-end code making AJAX requests to certain routes, and
-// and will need to make our own requests to our database.
-
-// function in db/index.js that we can use in db/seed.js
-
-async function createUser({ username, password, name, location }) {
+async function getAllUsers() {
   try {
-    const { rows } = await client.query(
-      `
-      INSERT INTO users(username, password, name, location)
-      VALUES ($1, $2, $3, $4)
-      ON CONFLICT (username) DO NOTHING 
-      RETURNING *;
-    `,
-      [username, password, name, location]
-    );
+    const { rows } = await client.query(`
+      SELECT id, username, name, location, active 
+      FROM users;
+    `);
 
     return rows;
   } catch (error) {
@@ -85,19 +69,46 @@ async function createUser({ username, password, name, location }) {
   }
 }
 
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+      SELECT id, username, name, location, active
+      FROM users
+      WHERE id=${userId}
+    `);
+
+    if (!user) {
+      return null;
+    }
+
+    user.posts = await getPostsByUser(userId);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * POST Methods
+ */
+
 async function createPost({ authorId, title, content }) {
   try {
-    const { rows } = await client.query(
+    const {
+      rows: [post],
+    } = await client.query(
       `
-      INSERT INTO users(authorId, title, content)
-      VALUES ($1, $2, $3, $4)
-      ON CONFLICT (authorId) DO NOTHING 
+      INSERT INTO posts("authorId", title, content) 
+      VALUES($1, $2, $3)
       RETURNING *;
     `,
       [authorId, title, content]
     );
 
-    return rows;
+    return post;
   } catch (error) {
     throw error;
   }
@@ -116,7 +127,7 @@ async function updatePost(id, fields = {}) {
 
   try {
     const {
-      rows: [posts],
+      rows: [post],
     } = await client.query(
       `
       UPDATE posts
@@ -125,10 +136,9 @@ async function updatePost(id, fields = {}) {
       RETURNING *;
     `,
       Object.values(fields)
-      // this expression returns an array
     );
 
-    return posts;
+    return post;
   } catch (error) {
     throw error;
   }
@@ -136,10 +146,10 @@ async function updatePost(id, fields = {}) {
 
 async function getAllPosts() {
   try {
-    const { rows } = await client.query(
-      `SELECT id from post;
-    `
-    );
+    const { rows } = await client.query(`
+      SELECT *
+      FROM posts;
+    `);
 
     return rows;
   } catch (error) {
@@ -149,8 +159,9 @@ async function getAllPosts() {
 
 async function getPostsByUser(userId) {
   try {
-    const { rows } = client.query(`
-      SELECT * FROM posts
+    const { rows } = await client.query(`
+      SELECT * 
+      FROM posts
       WHERE "authorId"=${userId};
     `);
 
@@ -160,28 +171,27 @@ async function getPostsByUser(userId) {
   }
 }
 
-async function getPostsById(userId) {
-  try {
-    const { rows } = client.query(`
-      SELECT * FROM posts
-      WHERE "authorId"=${userId};
-    `);
+module.exports = {
+  client,
+  createUser,
+  updateUser,
+  getAllUsers,
+  getUserById,
+  createPost,
+  updatePost,
+  getAllPosts,
+  getPostsByUser,
+};
 
-    return rows;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function getUserById(userId) {
-  // first get the user (NOTE: Remember the query returns
-  // (1) an object that contains
-  // (2) a `rows` array that (in this case) will contain
-  // (3) one object, which is our user.
-  // if it doesn't exist (if there are no `rows` or `rows.length`), return null
-  // if it does:
-  // delete the 'password' key from the returned object
-  // get their posts (use getPostsByUser)
-  // then add the posts to the user object with key 'posts'
-  // return the user object
-}
+// async function getUserById(userId) {
+//   // first get the user (NOTE: Remember the query returns
+//   // (1) an object that contains
+//   // (2) a `rows` array that (in this case) will contain
+//   // (3) one object, which is our user.
+//   // if it doesn't exist (if there are no `rows` or `rows.length`), return null
+//   // if it does:
+//   // delete the 'password' key from the returned object
+//   // get their posts (use getPostsByUser)
+//   // then add the posts to the user object with key 'posts'
+//   // return the user object
+// }
